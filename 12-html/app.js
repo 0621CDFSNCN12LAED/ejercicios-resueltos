df1b1cc1f31c905ba3549f254a6641910dd1d20c
Pablo Baleztena <pablo@ultharsoftware.com>
2021-08-04 17:56:56 -0300

    clase 12

---
+++ 12-html/app.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const app = express();
+
+const path = require("path");
+
+app.listen(3000, () => {
+  console.log("Se prendió!");
+});
+
+//Metodos de HTTP
+// - GET
+// - POST
+// - PUT
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "views/index.html"));
+});
